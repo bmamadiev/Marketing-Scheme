8b711406a7e7d224edae610721f1e1194f5dffd1
BEHZOD MAMADIEV <BMAMADIEV@GMAIL.com>
2022-08-19 19:42:39 -0400

    new update

--- buildScripts/runIntegrationTests.sh
+++ buildScripts/runIntegrationTests.sh
@@ -1,9 +1,10 @@
+#!/bin/bash
 failures=0
 trap 'failures=$((failures+1))' ERR
 ./gradlew marketing-integration-task1
-#./gradlew marketing-integration-task2
-#./gradlew marketing-integration-task3
-#./gradlew marketing-integration-task4
+./gradlew marketing-integration-task2
+./gradlew marketing-integration-task3
+./gradlew marketing-integration-task4
 if ((failures == 0)); then
   echo "Success"
 else
